793e63a42111cffbb78d558d6f856a5713990774
CoffeeStraw <16942527+CoffeeStraw@users.noreply.github.com>
2019-09-07 12:31:10 +0200

    Implemented State Machine to Control Attack Animations

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -12,11 +12,12 @@ export (float, 1.0, 100.0) var gravity_mult = 3
 # Variables
 var _camera
 var _character
-var _anim
+var _anim_attacks
 var _anim_tree
 
 var _hud_damage
 var _hud_speed
+var _hud_type
 
 var _speed = normal_speed
 var _velocity = Vector3()
@@ -24,12 +25,14 @@ var _walk_run_blend = 0.0
 var gravity = -9.81
 
 func _ready():
-	_camera     = $Target/Camera
-	_character  = $"Character Models and Stuff"
-	_anim       = $"Character Models and Stuff/Armature/AnimationPlayer"
-	_anim_tree  = $"Character Models and Stuff/Armature/AnimationTree"
+	_camera       = $Target/Camera
+	_character    = $"Character Models and Stuff"
+	_anim_tree    = $"Character Models and Stuff/Armature/AnimationTree"
+	_anim_attacks = _anim_tree['parameters/Attacks/playback']
+	
 	_hud_damage = $"Control/Panel/DamageValue"
 	_hud_speed  = $"Control/Panel/SpeedValue"
+	_hud_type  = $"Control/Panel/TypeValue"
 	
 	gravity *= gravity_mult
 	
@@ -93,21 +96,32 @@ func _physics_process(delta):
 	_anim_tree.set("parameters/Walk_Run/blend_amount", _walk_run_blend)
 
 func _on_SwipeDetector_swiped(gesture):
+	# Saving animation name
+	var anim_name = "attack_" + str( gesture.get_direction() )
+	
 	# Calculating power (used for later calculations of speed and damage)
 	var power = gesture.first_point().distance_to( gesture.last_point() )
 	var max_power = Vector2(0.0, 0.0).distance_to( get_viewport().size )
 	power /= max_power / 100
 	
-	# Calculating attack speed
-	var attack_speed = 1 + (100.0-power) * 3 / 100
+	# Calculating attack speed, normalizing all attack animations duration
+	var anim_speed_fix = 0.0
+	match anim_name:
+		"attack_left":
+			anim_speed_fix = 0.0
+		"attack_right":
+			anim_speed_fix = -0.2
+		"attack_down":
+			anim_speed_fix = -1.2
 	
-	_anim_tree.set("parameters/Attack/active", true)
+	var attack_speed = 1 + anim_speed_fix + (100.0-power) * 3 / 100
+	
+	# Enabling attack animation
+	_anim_attacks.start(anim_name)
+	_anim_tree.set("parameters/AttackShot/active", true)
 	_anim_tree.set("parameters/AttackSpeed/scale", attack_speed)
 	
+	# Setitng HUD
 	_hud_damage.set_text(str(int(power)))
-	_hud_speed.set_text(str(int(attack_speed)))
-	
-	print("Type of attack: " + str( gesture.get_direction() ) )
-	print("Attack power: " + str( power ) + " / 100.0" )
-	print("Attack speed: " + str( attack_speed ) )
-	print(" ")
+	_hud_speed.set_text(str(float(attack_speed)))
+	_hud_type.set_text(str(gesture.get_direction()))
